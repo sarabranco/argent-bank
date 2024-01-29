6f6f3e473b00de7a762cab3aee4e7fb6e339981c
sarabranco92 <sara1992@live.com.pt>
2024-01-29 18:15:14 +0100

    final

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,7 +9,7 @@ const store = configureStore({
     auth: authReducer, // Reducer pour la gestion de l'authentification
     user: userReducer, // Reducer pour la gestion des données utilisateur
   },
-  // Ajout d'options supplémentaires au besoin (middleware, devTools, etc.)
+
 });
 
 export default store;
